74b2bf4270aabc3104681cf27aca109eec39f499
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-07-26 14:52:39 -0300

    criando WebApiStartup

--- FortressAuth/FortressAuth/Program.cs
+++ FortressAuth/FortressAuth/Program.cs
@@ -1,11 +1,11 @@
+using FortressAuth;
+
 var builder = WebApplication.CreateBuilder(args);
 
+IServiceCollection services = builder.Services;
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.WebApiRegister();
 
 var app = builder.Build();
 
---
+++ FortressAuth/FortressAuth/WebApiStartup.cs
@@ -0,0 +1,15 @@
+﻿namespace FortressAuth
+{
+    public static class WebApiStartup
+    {
+        public static IServiceCollection WebApiRegister(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            serviceCollection.AddEndpointsApiExplorer();
+            serviceCollection.AddSwaggerGen();
+
+            return serviceCollection;
+        }
+    }
+}
