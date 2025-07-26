cf6296c2a619fc517f26d2422a214f08af87bb4b
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-07-26 17:35:28 -0300

    configuracao de instacia do DbContext no DI

---
+++ FortressAuth/FortressAuth.Infraestructure/Data/SqlServerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FortressAuth.Infraestructure.Data
+{
+    public class SqlServerDbContext : DbContext
+    {
+        public SqlServerDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SqlServerDbContext).Assembly);
+        }
+    }
+}
--- FortressAuth/FortressAuth/WebApiStartup.cs
+++ FortressAuth/FortressAuth/WebApiStartup.cs
@@ -1,13 +1,24 @@
-﻿namespace FortressAuth
+﻿using FortressAuth.Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FortressAuth
 {
     public static class WebApiStartup
     {
         public static IServiceCollection WebApiRegister(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             serviceCollection.AddEndpointsApiExplorer();
             serviceCollection.AddSwaggerGen();
+
+            serviceCollection.AddDbContext<SqlServerDbContext>((serviceProvider, dbContextOptionsBuilder) =>
+            {
+                IConfiguration configuration = serviceProvider.GetRequiredService<IConfiguration>();
+                var stringConnection = configuration.GetConnectionString("SqlServer");
+
+                dbContextOptionsBuilder.UseSqlServer(stringConnection);
+            });
 
             return serviceCollection;
         }
