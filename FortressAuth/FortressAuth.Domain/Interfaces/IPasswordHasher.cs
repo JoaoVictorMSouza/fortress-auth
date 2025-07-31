41111867869c547d45f0ef66221b83ff82bee163
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-07-30 21:30:47 -0300

    criando IPasswordHasher e BcryptPasswordHasher

---
+++ FortressAuth/FortressAuth.Domain/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿using FortressAuth.Domain.Entity;
+
+namespace FortressAuth.Domain.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool Verify(User user, string password);
+    }
+}
---
+++ FortressAuth/FortressAuth.Infraestructure/Security/BcryptPasswordHasher.cs
@@ -0,0 +1,18 @@
+﻿using FortressAuth.Domain.Entity;
+using FortressAuth.Domain.Interfaces;
+
+namespace FortressAuth.Infraestructure.Security
+{
+    public class BcryptPasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt(12));
+        }
+
+        public bool Verify(User user, string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, user?.PasswordHash);
+        }
+    }
+}
