04911a0f1c227838fb83e033f64935897c3c3979
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-07-31 19:55:12 -0300

    criando e configurando migration

---
+++ FortressAuth/FortressAuth.Infraestructure/Data/Migrations/20250731225027_create-users.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FortressAuth.Infraestructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class createusers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsInactive = table.Column<bool>(type: "bit", nullable: false),
+                    DhInclusion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DhChange = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ FortressAuth/FortressAuth.Infraestructure/Data/SqlServerDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace FortressAuth.Infraestructure.Data
+{
+    internal class SqlServerDbContextFactory : IDesignTimeDbContextFactory<SqlServerDbContext>
+    {
+        public SqlServerDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "FortressAuth");
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath) // << Correção aqui
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("SqlServer");
+
+            var optionsBuilder = new DbContextOptionsBuilder<SqlServerDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new SqlServerDbContext(optionsBuilder.Options);
+        }
+    }
+}
