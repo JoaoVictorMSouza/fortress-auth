aa600ba2301707ddf6c4ab2300a3bd07a3687d96
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-07-31 20:14:01 -0300

    criando mapeamento para user

---
+++ FortressAuth/FortressAuth.Application/DTOs/User/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FortressAuth.Application.DTOs.User
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ FortressAuth/FortressAuth.Application/Mappings/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FortressAuth.Application.DTOs.User;
+using FortressAuth.Domain.Entity;
+
+namespace FortressAuth.Application.Mappings
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<CreateUserDTO, User>()
+                .ForMember(user => user.Email, c => c.MapFrom(userDto => userDto.Email))
+                .ForMember(user => user.PasswordHash, c => c.MapFrom(userDto => userDto.Password))
+                .ForMember(user => user.Description, c => c.MapFrom(userDto => userDto.Description));
+        }
+    }
+}
