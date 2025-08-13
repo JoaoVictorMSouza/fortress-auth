e101c4607ac8f2678d35fbbec6c77e7be2af29e1
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-13 20:11:44 -0300

    implementando contrato GetAllUsersAsync da IUserRepository em UserRepository

--- FortressAuth/FortressAuth.Infraestructure/Data/UserRepository.cs
+++ FortressAuth/FortressAuth.Infraestructure/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using FortressAuth.Domain.Entity;
 using FortressAuth.Domain.Interfaces;
+using FortressAuth.Domain.ValueObjects.User;
 using Microsoft.EntityFrameworkCore;
 
 namespace FortressAuth.Infraestructure.Data
@@ -21,6 +22,48 @@ namespace FortressAuth.Infraestructure.Data
         public Task<User?> GetUserByEmailAsync(string email)
         {
             return _sqlServerDbContext.Users.FirstOrDefaultAsync<User>(user => user.Email == email);
+        }
+
+        public Task<List<User>> GetAllUsersAsync(UserFilter userFilter)
+        {
+            IQueryable<User> query = _sqlServerDbContext.Users;
+
+            if (userFilter.Id.HasValue)
+            {
+                query = query.Where(user => user.Id == userFilter.Id.Value);
+            }
+
+            if (!string.IsNullOrEmpty(userFilter.Name))
+            {
+                query = query.Where(user => user.Name.ToUpper() == userFilter.Name.ToUpper());
+            }
+
+            if (!string.IsNullOrEmpty(userFilter.Email))
+            {
+                query = query.Where(user => user.Email.ToUpper() == userFilter.Email.ToUpper());
+            }
+
+            if (userFilter.DhInclusionGreaterThan.HasValue)
+            {
+                query = query.Where(user => user.DhInclusion >= userFilter.DhInclusionGreaterThan.Value);
+            }
+
+            if (userFilter.DhInclusionLessThan.HasValue)
+            {
+                query = query.Where(user => user.DhInclusion <= userFilter.DhInclusionLessThan.Value);
+            }
+
+            if (userFilter.DhChangeGreaterThan.HasValue)
+            {
+                query = query.Where(user => user.DhChange >= userFilter.DhChangeGreaterThan.Value);
+            }
+
+            if (userFilter.DhChangeLessThan.HasValue)
+            {
+                query = query.Where(user => user.DhChange <= userFilter.DhChangeLessThan.Value);
+            }
+
+            return query.ToListAsync();
         }
     }
 }
