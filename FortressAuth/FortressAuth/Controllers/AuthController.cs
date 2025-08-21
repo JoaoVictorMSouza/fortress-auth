484721209de71ae1842d838fa6950cce33046637
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-21 19:50:57 -0300

    criando AuthController e rota de login

---
+++ FortressAuth/FortressAuth/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using FortressAuth.Application.DTOs.Auth;
+using FortressAuth.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FortressAuth.Controllers
+{
+    [ApiController, Route("[controller]")]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login"), AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO dto)
+        {
+            var ip = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+
+            var loginResponseDTO = await _authService.LoginAsync(dto, ip);
+
+            return Ok(loginResponseDTO);
+        }
+    }
+}
