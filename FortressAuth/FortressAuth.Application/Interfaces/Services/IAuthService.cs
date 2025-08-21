004af5c83809120095cf31d192df0f8a5e050ee5
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-20 21:18:14 -0300

    criando IAuthService

---
+++ FortressAuth/FortressAuth.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using FortressAuth.Application.DTOs.Auth;
+
+namespace FortressAuth.Application.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<LoginResponseDTO> LoginAsync(LoginRequestDTO loginRequestDTO, string ip);
+    }
+}
