4c7caef847e2bc8cbdd9944161fcf36c34b0401b
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-21 19:45:43 -0300

    criando IJwtService

---
+++ FortressAuth/FortressAuth.Application/Interfaces/Services/IJwtService.cs
@@ -0,0 +1,12 @@
+﻿using FortressAuth.Domain.Entity;
+
+namespace FortressAuth.Application.Interfaces.Services
+{
+    public interface IJwtService
+    {
+        string GenerateAccessToken(User user, DateTime expiresAtUtc);
+        string GenerateRefreshToken();
+        public DateTime GetExpirationAcessToken();
+        public DateTime GetExpirationRefreshToken();
+    }
+}
