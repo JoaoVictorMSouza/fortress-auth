56ff2e03bdd86e26eebc93ddf444c19ef65fda6a
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-20 21:17:25 -0300

    criando classe de login response

---
+++ FortressAuth/FortressAuth.Application/DTOs/Auth/LoginResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace FortressAuth.Application.DTOs.Auth
+{
+    public class LoginResponseDTO
+    {
+        public LoginResponseDTO(string accessToken, long accessTokenExpiry, string refreshToken, long refreshTokenExpiry)
+        {
+            AccessToken = accessToken;
+            AccessTokenExpiry = accessTokenExpiry;
+            RefreshToken = refreshToken;
+            RefreshTokenExpiry = refreshTokenExpiry;
+        }
+        public string AccessToken { get; set; }
+        public long AccessTokenExpiry { get; set; }
+        public string RefreshToken { get; set; }
+        public long RefreshTokenExpiry { get; set; }
+    }
+}
