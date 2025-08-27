bd1dd094f52c84b020df02ae18bcd1eb74999663
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-27 20:51:16 -0300

    criando IRefreshTokenUserRepository

---
+++ FortressAuth/FortressAuth.Domain/Interfaces/IRefreshTokenUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using FortressAuth.Domain.Entities;
+
+namespace FortressAuth.Domain.Interfaces
+{
+    public interface IRefreshTokenUserRepository
+    {
+        Task<RefreshTokenUser?> GetRefreshTokenUserByTokenAsync(string token);
+        Task AddRefreshTokenUserAsync(RefreshTokenUser refreshToken);
+        Task RevokeRefreshTokenUserByTokenAsync(string token);
+        Task RevokeAllRefreshTokensUserByUserIdAsync(Guid userId);
+    }
+}
