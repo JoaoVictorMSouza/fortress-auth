f20eb03cda30b9c5cbd67dca9c27d6f8c28eee77
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-27 20:49:23 -0300

    criando RefreshTokenUser

---
+++ FortressAuth/FortressAuth.Domain/Entities/RefreshTokenUser.cs
@@ -0,0 +1,24 @@
+﻿using FortressAuth.Domain.Entities.Base;
+using FortressAuth.Domain.Entity;
+
+namespace FortressAuth.Domain.Entities
+{
+    public class RefreshTokenUser : DefaultEntityGuid
+    {
+        public string Token { get; private set; }
+        public DateTime ExpiresAtUtc { get; private set; }
+        public bool Revoked { get; private set; }
+        public string? ReplacedByToken { get; private set; }
+
+        public User User { get; private set; }
+
+        public bool IsActive => !Revoked && DateTime.UtcNow < ExpiresAtUtc;
+
+        public void SetRevoked(string? replacedByToken = null)
+        {
+            Revoked = true;
+            Token = null!;
+            ReplacedByToken = replacedByToken;
+        }
+    }
+}
