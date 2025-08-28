5b1b6f7f202c3397191684f268f4d9be100fe056
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-27 21:34:24 -0300

    criando RefreshTokenResponseDTO

---
+++ FortressAuth/FortressAuth.Application/DTOs/Auth/RefreshTokenResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace FortressAuth.Application.DTOs.Auth
+{
+    public class RefreshTokenResponseDTO
+    {
+        public RefreshTokenResponseDTO(string accessToken, long accessTokenExpiry, string refreshToken, long refreshTokenExpiry)
+        {
+            AccessToken = accessToken;
+            AccessTokenExpiry = accessTokenExpiry;
+            RefreshToken = refreshToken;
+            RefreshTokenExpiry = refreshTokenExpiry;
+        }
+
+        public string AccessToken { get; set; }
+        public long AccessTokenExpiry { get; set; }
+        public string RefreshToken { get; set; }
+        public long RefreshTokenExpiry { get; set; }
+    }
+}
