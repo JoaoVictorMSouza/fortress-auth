b73d951d70e98593ceae2ca3fc89f371c4efd869
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-28 17:41:12 -0300

    criando meotodo RefreshAsync no IAuthService

--- FortressAuth/FortressAuth.Application/Interfaces/Services/IAuthService.cs
+++ FortressAuth/FortressAuth.Application/Interfaces/Services/IAuthService.cs
@@ -5,5 +5,6 @@ namespace FortressAuth.Application.Interfaces.Services
     public interface IAuthService
     {
         Task<LoginResponseDTO> LoginAsync(LoginRequestDTO loginRequestDTO, string ip);
+        Task<RefreshTokenResponseDTO> RefreshAsync(RefreshTokenRequestDTO refreshTokenRequestDTO);
     }
 }
