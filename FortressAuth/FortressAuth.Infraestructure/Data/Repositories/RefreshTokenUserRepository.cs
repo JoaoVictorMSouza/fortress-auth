c7d9a521ba072bbd97d2a54d840d7cee6615b634
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-28 17:42:58 -0300

    criando RefreshTokenUserRepository

---
+++ FortressAuth/FortressAuth.Infraestructure/Data/Repositories/RefreshTokenUserRepository.cs
@@ -0,0 +1,47 @@
+﻿using FortressAuth.Domain.Entities;
+using FortressAuth.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FortressAuth.Infraestructure.Data.Repositories
+{
+    public class RefreshTokenUserRepository : IRefreshTokenUserRepository
+    {
+        private SqlServerDbContext _sqlServerDbContext;
+
+        public RefreshTokenUserRepository(SqlServerDbContext sqlServerDbContext)
+        {
+            _sqlServerDbContext = sqlServerDbContext;
+        }
+
+        public Task AddRefreshTokenUserAsync(RefreshTokenUser refreshToken)
+        {
+            _sqlServerDbContext.RefreshTokensUser.Add(refreshToken);
+            return _sqlServerDbContext.SaveChangesAsync();
+        }
+
+        public Task<RefreshTokenUser?> GetRefreshTokenUserByTokenAndUserIdAsync(string token, Guid userId)
+        {
+            return _sqlServerDbContext.RefreshTokensUser.Include(x => x.User).FirstOrDefaultAsync(x => x.Token == token && x.UserId == userId);
+        }
+
+        public Task SetRevokedRefreshTokenUser(RefreshTokenUser refreshTokenUser)
+        {
+            _sqlServerDbContext.RefreshTokensUser.Update(refreshTokenUser);
+
+            return _sqlServerDbContext.SaveChangesAsync();
+        }
+
+        public Task SetRevokedAllRefreshTokensUserByUserIdAsync(Guid userId)
+        {
+            var refreshTokens = _sqlServerDbContext.RefreshTokensUser.Where(x => x.UserId == userId && !x.Revoked).ToList();
+
+            foreach (var refreshToken in refreshTokens)
+            {
+                refreshToken.SetRevoked();
+            }
+
+            _sqlServerDbContext.RefreshTokensUser.UpdateRange(refreshTokens);
+            return _sqlServerDbContext.SaveChangesAsync();
+        }
+    }
+}
