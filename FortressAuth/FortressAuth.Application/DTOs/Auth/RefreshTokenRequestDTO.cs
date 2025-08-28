aedc010a7de3b32a50447daf17846ca25f49eba2
JoaoVictorMSouza <vitormendes122016@gmail.com>
2025-08-28 19:53:25 -0300

    criando RefreshTokenRequestDTO

---
+++ FortressAuth/FortressAuth.Application/DTOs/Auth/RefreshTokenRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FortressAuth.Application.DTOs.Auth
+{
+    public class RefreshTokenRequestDTO
+    {
+        public string RefreshToken { get; set; }
+        public string AcessToken { get; set; }
+    }
+}
